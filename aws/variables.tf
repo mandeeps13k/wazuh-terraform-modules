bae147da9704a1f5267261fb9cea74445d1c30cb
Mandeep Singh Kapoor <mandeeps13k@gmail.com>
2023-04-14 02:13:21 +0530

    Create variables.tf

---
+++ aws/variables.tf
@@ -0,0 +1,41 @@
+variable "delivery_stream_name" {
+  type        = string
+  default     = "wazuh-guardduty-logs"
+  description = "The name of the Firehose Delivery Stream."
+}
+
+variable "bucket_prefix" {
+  type        = string
+  default     = ""
+  description = "AWS account name to receive guard duty alerts"
+}
+
+variable "admin_account_id" {
+  type        = string
+  default     = ""
+  description = "The AWS account ID to manage admin components."
+}
+
+variable "admin_account_region" {
+  type        = string
+  default     = "ap-south-1"
+  description = "The AWS region to manage admin components."
+}
+
+variable "guardduty-events-bucket-name" {
+  type        = string
+  default     = "wazuh-guardduty-findings"
+  description = "The name of the S3 bucket to store GuardDuty findings in the admin account."
+}
+
+variable "kms-events-bucket-name" {
+  type        = string
+  default     = "wazuh-kms-events"
+  description = "The name of the S3 bucket to store GuardDuty findings in the admin account."
+}
+
+variable "delivery_stream_name_kms" {
+  type        = string
+  default     = "wazuh-kms-events"
+  description = "The name of the Firehose Delivery Stream."
+}
